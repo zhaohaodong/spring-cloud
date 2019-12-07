c060bee7f64904c38dd1571527855a231ce22e25
zhaohaodong <32354104+zhaohaodong@users.noreply.github.com>
2019-12-07 15:45:43 +0800

    配置中心服务提交

---
+++ spring-cloud-config/spring-cloud-config-server/src/test/java/com/zhd/cloud/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.zhd.cloud.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
